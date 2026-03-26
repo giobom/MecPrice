553c3454016924439ad7f43b6ebda032781d4b36
giobom <giobom2@hotmail.com>
2026-03-26 20:41:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,12 @@
-const CACHE_NAME = "mecprice-v1.1.6";
-
+const CACHE_NAME = "mecprice-orcamento-v1,002";
 const URLS_TO_CACHE = [
   "./",
   "./index.html",
   "./style.css",
-  "./app.js",
+  "./script.js",
   "./manifest.json",
-
   "./icon-192.png",
-  "./icon-512.png",
-
-  // libs
-  "./libs/core.js",
-  "./libs/dom.js",
-  "./libs/storage.js",
-  "./libs/tabs.js",
-  "./libs/estoque.js",
-  "./libs/orcamento.js",
-  "./libs/pdf.js",
-  "./libs/validators.js",
-  "./libs/pro-modal.js",
-
-  // pdf libs
-  "./libs/jspdf.umd.min.js",
-  "./libs/jspdf.plugin.autotable.min.js"
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -32,26 +15,12 @@ self.addEventListener("install", (event) => {
       return cache.addAll(URLS_TO_CACHE);
     })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
